127ad0f5afbf6a27e0c99f079e39351be85ea13e
rubamajeed <rubaalmalkii2001@gmail.com>
2024-09-03 01:10:28 +0300

    Update assignment_1.py
    answer

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,20 @@
 def players_as_dictionaries(squads_list):
-    pass
+    SQUADS_DICT = []
+
+for player in SQUADS_DATA:
+    player_dict = {
+        'number': player[0],
+        'position': player[1],
+        'name': player[2],
+        'date_of_birth': player[3],
+        'caps': player[4],
+        'club': player[5],
+        'country': player[6],
+        'club_country': player[7],
+        'year': player[8],
+    }
+    SQUADS_DICT.append(player_dict)
+
+# Output the result
+print(SQUADS_DICT)
+
